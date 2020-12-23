a44fc0ad954f9cc5dc626861d553b80d36839b1a
gbox3d <gbox3d@gmail.com>
2020-12-23 12:10:50 +0900

    ...

--- redWine/index.js
+++ redWine/index.js
@@ -106,9 +106,8 @@ async function process_get(req, res) {
             }
             break;
 
-        //to do 
         //https://stackoverflow.com/questions/18894433/nodejs-child-process-working-directory
-        //실행디랙토리도 지정하게 해야함 
+        //cwd 실행디랙토리, 
         case '/rest/exec':
             {
 
